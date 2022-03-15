60f4ed192a3e8c6a7fbe426e72a63918b53772d3
Luke Michael Simmons <TheRealLukeSimmons@gmail.com>
2022-03-15 15:01:08 -0700

    Add files via upload

---
+++ data_processing/hybrid_groupby/agg_records.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+def hybrid_groupby(df, grp_cols, sum_cols=[], avg_cols=[], min_cols=[], max_cols=[]): 
+    """The purpose of this function is to replicate the functionality of a SQL GROUP BY clause. 
+       Takes a DataFrame, a list of grouping columns, a few lists of columns to operate on, and 
+       returns the first record of all ignored fields."""
+    
+    sum_df = df[grp_cols + sum_cols].groupby(grp_cols, as_index=False).sum()
+    avg_df = df[grp_cols + avg_cols].groupby(grp_cols, as_index=False).mean()
+    min_df = df[grp_cols + min_cols].groupby(grp_cols, as_index=False).min()
+    max_df = df[grp_cols + max_cols].groupby(grp_cols, as_index=False).max()
+    
+    agg_cols = sum_cols + avg_cols + min_cols + max_cols
+    df = df.drop_duplicates(subset=grp_cols, keep='first').copy()
+    df.drop(columns=agg_cols, inplace=True)
+    
+    df = df.merge(sum_df).merge(avg_df).merge(min_df).merge(max_df)
+    
+    return df
